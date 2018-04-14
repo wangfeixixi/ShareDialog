f33ad68a6da97720531f2f799a2dd74afa8d7bcb
xuanyuanxixi <1020301980@qq.com>
2018-04-14 13:10:06 +0800

    Initial commit

---
+++ app/src/main/java/wangfeixixi/github/com/widget_dialog/MainActivity.java
@@ -0,0 +1,13 @@
+package wangfeixixi.github.com.widget_dialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
